6c5ee4e3741d3f018b5f1be569fe3fabdb7cbf7a
Gabriel D'Luca <dlukagb1999@hotmail.com>
2017-07-04 05:33:02 -0300

    Update project.py

--- project.py
+++ project.py
@@ -8,7 +8,7 @@ import datetime
 birddata = pd.read_csv("bird_tracking.csv")
 bird_names = pd.unique(birddata.bird_name)    
 
-#Generates trajectory graph
+# PART 1 - TRAJECTORY
 plt.figure(figsize=(7,7))
 for bird in bird_names:
     ix = birddata.bird_name==bird
@@ -20,7 +20,7 @@ plt.ylabel("Latitude")
 plt.legend(loc="lower right")
 plt.savefig("trajectories.pdf")
 
-#Generates Eric's speed graph
+# PART 2 - ERIC'S SPEED
 ix = birddata.bird_name=='Eric'
 speed = birddata.speed_2d[ix]
 ind = np.isnan(speed)
@@ -34,7 +34,7 @@ plt.xlabel("2D speed (m/s)")
 plt.ylabel("Frequency")
 plt.savefig("eric_speed.pdf")
 
-#Generates timeplot graph
+# PART 3 - ERIC'S TIMEPLOT
 timestamps = []
 for k in range(len(birddata)):
     timestamps.append(datetime.datetime.strptime(birddata.date_time.iloc[k][:-3], "%Y-%m-%d %H:%M:%S"))
@@ -51,7 +51,7 @@ plt.xlabel("Observations")
 plt.ylabel("Elapsed time (days)")
 plt.savefig("eric_timeplot.pdf")
 
-#Generates Eric's migration pattern file
+# PART 4 - ERIC'S MIGRATION PATTERN
 next_day = 1
 inds = []
 speeds = []
@@ -69,7 +69,7 @@ plt.xlabel("Day")
 plt.ylabel("Mean speed (m/s)")
 plt.savefig("eric_pattern.pdf")
 
-#Generates the map file
+# PART 5 - THE MAP
 proj = ccrs.Mercator()
 
 plt.figure(figsize=(10,10))
@@ -88,7 +88,7 @@ for name in bird_names:
 plt.legend(loc="upper left")
 plt.savefig("map.pdf")
 
-#Generates pattern file for all birds
+# PART 6 - ALL BIRDS' MIGRATION PATTERN
 birddata.date_time = pd.to_datetime(birddata.date_time)
 birddata["date"] = birddata.date_time.dt.date
 
